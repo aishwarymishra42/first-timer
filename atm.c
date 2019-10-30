9cbb0c0e94cebd59aa66e9c48feed4b39561afed
aishwarymishra42 <57167396+aishwarymishra42@users.noreply.github.com>
2019-10-30 16:20:07 +0530

    atmfirst.c

--- atm.c
+++ atm.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 #include<conio.h>
 int main(){
-int a;
+int a=0;
 	while(a<=5){
 	printf("\n enter the amount in multiples of 100\n");
-	int n,m;
+	int n;
 	scanf("%d",&n);
 	if(n%100==0){
 		if(n/100>9){
 			printf("no. of thousand notes %d\n",n/1000);
 			printf("no. of five hundred notes %d\n",(n%1000)/500);
-            m=(n%1000)%500;
+            int	m=(n%1000)%500;
 			printf("no. of hundred notes %d\n",m/100);
 		}else if(n/100<=9){
 			printf("no. of five hundred notes %d\n",n/500);
